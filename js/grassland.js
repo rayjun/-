dfe694dc4955b4224b1aae4b2d5e863f14823a20
rayjun <rayjun0412@gmail.com>
2020-05-24 11:27:27 +0800

    Site updated: 2020-05-24 11:27:27

---
+++ js/grassland.js
@@ -0,0 +1,13 @@
+function highlightCode() {
+    var pres = document.querySelectorAll("figure");
+
+    for (var i = 0; i < pres.length; i++) {
+        hljs.highlightBlock(pres[i]);
+    }
+}
+highlightCode();
+
+
+function print(tags) {
+    console.log(tags);
+}
