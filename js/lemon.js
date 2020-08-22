053a432d672b8186baae3c2a280d25580aebc14c
rayjun <rayjun0412@gmail.com>
2020-08-22 23:00:32 +0800

    Site updated: 2020-08-22 23:00:30

--- js/lemon.js
+++ js/lemon.js
@@ -1,8 +1,29 @@
-function highlightCode() {
-    var pres = document.querySelectorAll("figure");
 
+function highlightCode() {
+    hljs.initHighlightingOnLoad();
+    var pres = document.querySelectorAll("figure");
+    hljs.configure({
+        languages: ["java", "javascript"]
+    });
     for (var i = 0; i < pres.length; i++) {
         hljs.highlightBlock(pres[i]);
     }
 }
 highlightCode();
+
+
+function getByClass(parent, cls){
+    if(parent.getElementsByClassName){
+      return parent.getElementsByClassName(cls);
+    }else{
+      var res = [];
+      var reg = new RegExp(' ' + cls + ' ', 'i')
+      var ele = parent.getElementsByTagName('*');
+      for(var i = 0; i < ele.length; i++){
+        if(reg.test(' ' + ele[i].className + ' ')){
+          res.push(ele[i]);
+        }
+      }
+      return res;
+    }
+  }
